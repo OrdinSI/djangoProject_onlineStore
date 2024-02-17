ad34cf309c7f5919f0d002f5a1195f81d4537d33
Ордин Сергей Игоревич <osi@maynitek.ru>
2024-02-17 14:35:20 +0500

    migrate_1

--- catalog/models.py
+++ catalog/models.py
@@ -25,12 +25,12 @@ class Product(models.Model):
     image_preview = models.ImageField(upload_to="products/", verbose_name="Preview image", **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Category")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
-    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Date added")
-    date_modified = models.DateTimeField(auto_now=True, verbose_name="Date modified")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date added")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Date modified")
 
     def __str__(self):
         return (f"{self.name}, {self.description}, "
-                f"{self.price}, {self.date_added}, {self.date_modified}, {self.category}")
+                f"{self.price}, {self.created_at}, {self.updated_at}, {self.category}")
 
     class Meta:
         verbose_name = "Product"
