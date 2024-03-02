dceedbc2ea4dcc228e8fc25b4f33fc858bc7b932
Ордин Сергей Игоревич <osi@maynitek.ru>
2024-03-02 12:58:54 +0500

    task_20.1_задание_3

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 
 from blog.apps import BlogConfig
 from django.urls import path
-from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, publish
 
 
 app_name = BlogConfig.name
@@ -12,5 +12,6 @@ urlpatterns = [
     path("blog_detail/<int:pk>/", BlogDetailView.as_view(), name='blog_detail'),
     path("blog_delete/<int:pk>/", BlogDeleteView.as_view(), name='blog_delete'),
     path("blog_update/<int:pk>/", BlogUpdateView.as_view(), name='blog_update'),
+    path("publish/<int:pk>/", publish, name='publish'),
 ]
 
